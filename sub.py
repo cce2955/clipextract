59cc5f023711a9628231de04f7475521cc0f0cab
Christopher Everett <cce29555@gmail.com>
2023-10-31 14:55:35 -0400

    added time range and twitch support
    Twitch wasn't actually working, fixed that by adding mp4 support and h.264 support

--- sub.py
+++ sub.py
@@ -11,11 +11,13 @@ search_word = input("Enter the word to search for: ")
 if not os.path.exists("sub"):
     os.makedirs("sub")
 
-# Step 1: Download the video using yt-dlp
-os.system(f"yt-dlp {video_url} -o video.%(ext)s")
+# Step 1: Download the video using yt-dlp prioritizing H.264 codec
+start_time = input("Enter the start time in seconds: ")
+end_time = input("Enter the end time in seconds: ")
+os.system(f'yt-dlp {video_url} -f bestvideo[ext=mp4][vcodec=avc1]+bestaudio[ext=m4a]/best[ext=mp4] --external-downloader ffmpeg --external-downloader-args "ffmpeg_i:-ss {start_time} -to {end_time}" -o video.mp4')
 
 # Step 2: Convert the downloaded video to .wav audio format using ffmpeg
-subprocess.call(['ffmpeg', '-i', 'video.webm', 'audio.wav'])
+subprocess.call(['ffmpeg', '-i', 'video.mp4', 'audio.wav'])
 
 # Step 3: Split the audio file into chunks
 audio = AudioSegment.from_wav("audio.wav")
@@ -24,7 +26,6 @@ chunks = [audio[i:i + chunk_length_ms] for i in range(0, len(audio), chunk_lengt
 
 # Step 4: Transcribe each chunk and search for the specified word
 def transcribe_and_search(audio_chunk, chunk_number):
-    # Save the audio chunk to a temporary file
     temp_filename = f'temp_chunk{chunk_number}.wav'
     audio_chunk.export(temp_filename, format="wav")
     
@@ -34,65 +35,53 @@ def transcribe_and_search(audio_chunk, chunk_number):
         try:
             transcription = r.recognize_google(audio_data)
             if search_word.lower() in transcription.lower():
-                # Calculate the start and end times for the video clip
-                start_time = chunk_number  # start time in seconds
-                end_time = chunk_number + 1  # end time in seconds
+                start_time = chunk_number
+                end_time = chunk_number + 1
                 
-                                # Extract the video clip using ffmpeg
                 output_filename = f'sub/chunk{chunk_number}.mp4'
                 subprocess.call([
-                    'ffmpeg',
-                    '-y',  # Automatically overwrite output file without asking
-                    '-i', 'video.webm',
+                    'ffmpeg', '-y',
+                    '-i', 'video.mp4',
                     '-ss', str(start_time),
                     '-to', str(end_time),
-                    '-c:v', 'libx264',  # Copy video stream as is
-                    '-c:a', 'aac',   # Convert audio stream to AAC
-                    '-strict', 'experimental',  # This might not be necessary if using a newer version of FFmpeg
+                    '-c:v', 'libx264',
+                    '-c:a', 'aac',
                     output_filename
                 ])
-
-                
                 print(f"Word found in chunk {chunk_number}. Video saved to {output_filename}")
+        except sr.UnknownValueError:
+            pass
         except sr.RequestError as e:
             print(f"Could not request results from Google Speech Recognition service; {e}")
-        except sr.UnknownValueError:
-            pass  # No transcription available for this chunk
         
-    # Delete the temporary file
     os.remove(temp_filename)
 
-# Transcribe each chunk and search for the specified word
 for i, chunk in enumerate(chunks):
     transcribe_and_search(chunk, i)
+
 def merge_clips():
-    # Step 1: Prompt the user
     user_input = input("Do you want to merge all clips into a single file? (y/n): ").lower()
     if user_input == 'y':
-        # Step 2: Create a text file listing all the video files
         with open('filelist.txt', 'w') as file:
-            for i in range(100):  # Assuming there are 100 chunks; adjust as needed
+            for i in range(len(chunks)):
                 chunk_file = f'sub/chunk{i}.mp4'
                 if os.path.exists(chunk_file):
                     file.write(f"file '{chunk_file}'\n")
 
-        # Step 3: Use FFmpeg to merge the video files
         subprocess.call([
-            'ffmpeg',
-            '-y',  # Automatically overwrite output file without asking
+            'ffmpeg', '-y',
             '-f', 'concat',
             '-safe', '0',
             '-i', 'filelist.txt',
             '-c', 'copy',
-            'output/merged_output.mp4'
+            'merged_output.mp4'
         ])
         print("Merging completed. The merged file is saved as merged_output.mp4.")
 
-# Call the function to execute the merging process
 merge_clips()
 
-if os.path.exists("Audio.wav"):
-    os.remove("Audio.wav")
+if os.path.exists("audio.wav"):
+    os.remove("audio.wav")
 
-if os.path.exists("Video.webm"):
-    os.remove("Video.webm")
+if os.path.exists("video.mp4"):
+    os.remove("video.mp4")
